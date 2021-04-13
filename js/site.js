c6b243a873a4a09916ba1f84c6a688aa46abb8a4
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-12 22:34:55 -0700

    Update 27
    Updated HTML, CSS, and JS and also attempted to add waypoint animation

--- js/site.js
+++ js/site.js
@@ -2,3 +2,9 @@
 //Author: CST 252 Group 7
 //Created: April 12th 2021
 //License: Public Domain
+
+var $mediabody = $('.mediabody');
+
+$mediabody.waypoint(function(){
+  $mediabody.addClass('js-mediabody-animate');
+}, {offset: '50%'});
