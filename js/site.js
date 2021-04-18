a2d25ebf5110a506b1ebe330c56e1d1b7b570432
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-17 20:32:18 -0700

    Update 4
    added hide/ show elements button

--- js/site.js
+++ js/site.js
@@ -7,10 +7,21 @@ var $mediabody = $('.mediabody');
 
 $mediabody.waypoint(function(){
   $mediabody.addClass('js-mediabody-animate');
-}, {offset: '50%'});
+}, {offset: '40%'});
 
 // var $mediabody2 = $('.mediabody2');
 //
 // $mediabody.waypoint(function(){
 //   $mediabody.addClass('js-mediabody-animate2');
 // }, {offset: '50%'});
+
+
+//Hide / Display elements
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
